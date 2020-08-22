9037c4963c95a1919135583ebc36bb831d4eaff3
Liu Shuiyuan <1210257083@qq.com>
2020-08-22 11:06:53 +0800

    second

--- toy-react.js
+++ toy-react.js
@@ -7,9 +7,13 @@ class ElementWrapper {
   
       setAttribute(name, value) {
           if (name.match(/^on([\s\S]+)$/)) {
-             this.root.addEventListener(RegExp.$1.replace(/^[\s\S]$/, c => c.toLowerCase()), value)
+             this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
           } else {
-             this.root.setAttribute(name, value);
+             if (name === "className") {
+                  this.root.setAttribute('class', value);                 
+             } else {
+                  this.root.setAttribute(name, value);
+             }
           }
   
       }
@@ -53,8 +57,15 @@ export class Component {
           this.render()[RENDER_TO_DOM](range)
       }
       rerender() {
-            this._range.deleteContents()
-            this[RENDER_TO_DOM](this._range)
+            let oldRange = this._range
+
+            let range = document.createRange()
+            range.setStart(oldRange.startContainer, oldRange.startOffset)
+            range.setEnd(oldRange.startContainer, oldRange.startOffset)
+            this[RENDER_TO_DOM](range)
+            
+            oldRange.setStart(range.endContainer, range.endOffset)
+            oldRange.deleteContents()
       }
       setState(newState) {
             if (this.state === null || typeof this.state !== 'object') {
@@ -94,7 +105,10 @@ export function createElement(type, attributes, ...children) {
                   if (typeof child === 'string') {
                         child = new TextWrapper(child)
                   }
-                  if(typeof child === 'object' && child instanceof Array) {
+                  if (child === null) {
+                        continue
+                  }
+                  if (typeof child === 'object' && child instanceof Array) {
                         insertChihld(child)
                   } else {
                         e.appendChild(child)
